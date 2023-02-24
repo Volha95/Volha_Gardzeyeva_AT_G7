9242b61881e27ffd248e6aecc765201cf8762f11
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-02-24 22:07:28 +0300

    added methods to the water

--- src/main/java/project/soda/Water.java
+++ src/main/java/project/soda/Water.java
@@ -1,10 +1,10 @@
 package project.soda;
 
 abstract class Water {
-    private String color;
-    private String transparency;
-    private String smell;
-    private int temperature;
+    private String color = "no";
+    private String transparency = "transparent";
+    private String smell = "no";
+    private int temperature = 0;
 
     public Water(String color, String transparency, String smell, int temperature) {
         this.color = color;
@@ -12,5 +12,38 @@ abstract class Water {
         this.smell = smell;
         this.temperature = temperature;
     }
-}
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTransparency() {
+        return transparency;
+    }
+
+    public void setTransparency(String transparency) {
+        this.transparency = transparency;
+    }
+
+    public String getSmell() {
+        return smell;
+    }
+
+    public void setSmell(String smell) {
+        this.smell = smell;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+
+}
