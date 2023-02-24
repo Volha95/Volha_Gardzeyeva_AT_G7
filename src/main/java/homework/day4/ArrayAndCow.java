91d7e0af93960947eac207c41ff9c9acaafa6ed7
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-02-24 16:24:20 +0300

    I have finished array subtusk3

--- src/main/java/homework/day4/ArrayAndCow.java
+++ src/main/java/homework/day4/ArrayAndCow.java
@@ -8,7 +8,8 @@ public class ArrayAndCow {
         ArrayAndCow object = new ArrayAndCow();
         int[] myArray = {1, 14, 15, 20, 27, 28, 30, 31, 74, 80};
         object.tot(myArray, 4);
-        System.out.println(Arrays.toString(createArray(myArray,20)));
+        System.out.println(Arrays.toString(createArray(myArray, 20)));
+        System.out.println("сумма равна: " + monthArray(myArray));
 
     }
 
@@ -19,18 +20,31 @@ public class ArrayAndCow {
         }
         System.out.println("Сумма элементов массива: " + sum);
     }
+
     public static int[] createArray(int[] array, int n) {
         ArrayList<Integer> myArrayList = new ArrayList<>();
-        for (int i=0; i< array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] > n) {
                 myArrayList.add(array[i]);
             }
         }
         int[] newArray = new int[myArrayList.size()];
-        for (int i=0; i< newArray.length; i++) {
+        for (int i = 0; i < newArray.length; i++) {
             newArray[i] = myArrayList.get(i);
         }
         return newArray;
+    }
+
+    public static int monthArray(int[] array) {
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % month == 0){
+                sum += array[i];
+            }
+        }
+        return sum;
     }
 
 
