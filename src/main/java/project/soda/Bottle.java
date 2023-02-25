1e6d35320e5c235d78eecd6ed871532774c8acd6
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-02-26 00:07:03 +0300

    updated the Bottle

--- src/main/java/project/soda/Bottle.java
+++ src/main/java/project/soda/Bottle.java
@@ -3,14 +3,16 @@ package project.soda;
 public class Bottle {
     private double volume;
     private Water water;
-    Bubble[] bubbles;
 
     Bottle(double volume) {
         Bubble[] bubbles = new Bubble[(int) (10000 * volume)];
         for (int i = 0; i < bubbles.length; i++) {
             bubbles[i] = new Bubble("CO2");
         }
-        this.bubbles = bubbles;
+    }
+
+    public void open() {
+        water.degas();
     }
 
     public Water getWater() {
@@ -20,12 +22,11 @@ public class Bottle {
     public void setWater(Water water) {
         this.water = water;
     }
-    public void open() {
-    this.water.setOpened(true);
-    }
-    public void warm(int temperature) {
-        Water temp = new Water() {
-            private int temperature = temp.getTemperature();
-        }
-    }
 }
+
+//    public void warm(int temperature) {
+//        Water temp = new Water() {
+//            private int temperature = temp.getTemperature();
+//        }
+//    }
+
