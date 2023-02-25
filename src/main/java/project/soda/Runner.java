c328d2f5df71d66311cec20f33d65329c4d70467
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-02-26 00:06:38 +0300

    updated the Runner

--- src/main/java/project/soda/Runner.java
+++ src/main/java/project/soda/Runner.java
@@ -2,19 +2,15 @@ package project.soda;
 
 public class Runner {
     public static void main(String[] args) {
-        SparklingWater water1 = new SparklingWater("green", "transparent", "odorless", 21);
-        SparklingWater water2 = new SparklingWater("red", "cloudy", "sweet", 22);
-        SparklingWater water3 = new SparklingWater("yellow", "transparent", "odorless", 23);
+        SparklingWater water1 = new SparklingWater();
+        SparklingWater water2 = new SparklingWater();
+        SparklingWater water3 = new SparklingWater();
         Bottle bottle1 = new Bottle(0.5);
-        Bottle bottle2 = new Bottle(1.);
+        Bottle bottle2 = new Bottle(1.0);
         Bottle bottle3 = new Bottle(1.5);
 
-        water1.pump(bottle1.bubbles);
-        water2.pump(bottle2.bubbles);
-        water3.pump(bottle3.bubbles);
-
-        bottle1.open(water1);
-        bottle2.open(water2);
-        bottle3.open(water3);
+        bottle1.open();
+        bottle2.open();
+        bottle3.open();
     }
 }
