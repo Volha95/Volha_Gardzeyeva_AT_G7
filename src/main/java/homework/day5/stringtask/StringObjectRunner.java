3f74db338b99f38bc34864671fe0917e043c5c66
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-03 22:07:02 +0300

    added stringtask 1.1

---
+++ src/main/java/homework/day5/stringtask/StringObjectRunner.java
@@ -0,0 +1,8 @@
+package homework.day5.stringtask;
+
+public class StringObjectRunner {
+    public static void main(String[] args) {
+        LettersColumn lettersColumn = new LettersColumn();
+        lettersColumn.printLettersColumn("Hello");
+    }
+}
