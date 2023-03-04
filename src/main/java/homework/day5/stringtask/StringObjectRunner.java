b4a2e1dbfc838df0e594915dfd39c062a314d229
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-04 12:52:57 +0300

    added stringtask 1.3

--- src/main/java/homework/day5/stringtask/StringObjectRunner.java
+++ src/main/java/homework/day5/stringtask/StringObjectRunner.java
@@ -4,5 +4,9 @@ public class StringObjectRunner {
     public static void main(String[] args) {
         LettersColumn lettersColumn = new LettersColumn();
         lettersColumn.printLettersColumn("Hello");
+        DuplicateWords duplicateWords = new DuplicateWords();
+        duplicateWords.printDuplicateWords("Big black bug bit a big black dog on his big black nose");
+        ArrayNumericVariables arrayNumericVariables = new ArrayNumericVariables();
+        arrayNumericVariables.printArray("123");
     }
 }
