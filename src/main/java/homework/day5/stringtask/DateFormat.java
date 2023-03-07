e3e1d174d242692d7eec354678aca06869c33b43
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-07 10:54:44 +0300

    finished stringtask 1.6

--- src/main/java/homework/day5/stringtask/DateFormat.java
+++ src/main/java/homework/day5/stringtask/DateFormat.java
@@ -1,8 +1,19 @@
 package homework.day5.stringtask;
 
-public class DateFormat {
-     String text = "k.mm dd.MM.yyyy";
-     public void printFormatDate(String text) {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-     }
+public class DateFormat {
+    String date = "22.00 07.09.2020";
+
+    public void printFormatDate(String date) throws ParseException {
+        SimpleDateFormat oldDateFormat = new SimpleDateFormat("HH.mm dd.MM.yyyy", Locale.ENGLISH);
+        SimpleDateFormat newDateFormat = new SimpleDateFormat("MMMM, d, yyyy HH:mm", Locale.ENGLISH);
+        Date date1 = oldDateFormat.parse(date);
+        String result = newDateFormat.format(date1);
+        System.out.println(result);
+    }
 }
+
--- src/main/java/homework/day5/stringtask/StringObjectRunner.java
+++ src/main/java/homework/day5/stringtask/StringObjectRunner.java
@@ -1,12 +1,17 @@
 package homework.day5.stringtask;
 
+
+import java.text.ParseException;
+
 public class StringObjectRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         LettersColumn lettersColumn = new LettersColumn();
         lettersColumn.printLettersColumn("Hello");
         DuplicateWords duplicateWords = new DuplicateWords();
         duplicateWords.printDuplicateWords("Big black bug bit a big black dog on his big black nose");
         ArrayNumericVariables arrayNumericVariables = new ArrayNumericVariables();
         arrayNumericVariables.printArray("123");
+        DateFormat dateFormat = new DateFormat();
+        dateFormat.printFormatDate("22.00 07.09.2020");
     }
 }
