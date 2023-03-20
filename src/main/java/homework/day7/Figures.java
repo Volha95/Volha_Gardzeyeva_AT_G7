d068f6063f2a51c3d3de4f26472c259d78828203
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-20 11:28:12 +0300

    added homework task 2

---
+++ src/main/java/homework/day7/Figures.java
@@ -0,0 +1,42 @@
+package homework.day7;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Figures {
+    public static void main(String[] args) {
+
+        List<String> figures = Arrays.asList("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс");
+        File file = new File("figures.txt");
+        int count = 0;
+
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(file));
+            for (String figure : figures) {
+                out.write(figure + " ");
+            }
+        } catch (IOException e) {
+            System.out.println();
+        }
+        for (String figure : figures) {
+            if (!figure.contains("и")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        for (int i = 0; i < figures.size(); i++) {
+            System.out.print(figures.get(i) + " ");
+        }
+        System.out.println();
+        figures.set(2, "Треугольник");
+
+        for (String figure : figures) {
+            System.out.print(figure + " ");
+        }
+    }
+}
