5eaefdf64e151f3b1a2e3280587863c41dca4161
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-21 13:11:25 +0300

    added hometask collections 8

---
+++ src/main/java/homework/day7/NumbersList.java
@@ -0,0 +1,36 @@
+package homework.day7;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class NumbersList {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3342, 34, 79, 23426, 68, 1324, 55, 7699);
+
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+
+        int sum = 0;
+        for (Integer number : numbers) {
+            for (int i = 0; i < number; i++) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+
+        Collections.sort(numbers);
+
+        for (int i = 0; i < numbers.size(); i++){
+            System.out.print(numbers.get(i) + " ");
+        }
+        System.out.println();
+
+        Collections.reverse(numbers);
+
+        for (Integer number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
