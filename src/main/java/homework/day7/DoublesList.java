7aaffce7d86e51276647f3fd5cabf74760af135e
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-21 22:00:45 +0300

    corrected the sum of fractional parts

--- src/main/java/homework/day7/DoublesList.java
+++ src/main/java/homework/day7/DoublesList.java
@@ -7,7 +7,7 @@ public class DoublesList {
     public static void main(String[] args) {
         List<Double> doubles = Arrays.asList(33.42, 34.3, 0.79, 2.3426, 6.8, 13.24, 5.5, 769.9);
 
-        for (Double doubl : doubles) {
+        for (double doubl : doubles) {
             System.out.print(doubl + " ");
         }
         System.out.println();
@@ -18,14 +18,9 @@ public class DoublesList {
         }
         System.out.println(x);
 
-        int sum = 0;
-        for (Double doubl : doubles) {
-            String numberStr = Double.toString(doubl);
-            String fractionalStr = numberStr.substring(numberStr.indexOf('.') + 1);
-            int fractional = Integer.valueOf(fractionalStr);
-            for (int i = 0; i < fractional; i++) {
-                sum++;
-            }
+        double sum = 0;
+        for (double doubl : doubles) {
+            sum = sum + (doubl - (int) doubl);
         }
         System.out.println(sum);
 
