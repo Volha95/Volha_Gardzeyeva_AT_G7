15c7bf40cbb349ad7a637c90e2a6cbcab3e02b13
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-22 14:46:27 +0300

    finished task 12

---
+++ src/main/java/homework/day7/PersonList.java
@@ -0,0 +1,29 @@
+package homework.day7;
+
+import homework.day7.objects.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonList {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(32, "Коля"));
+        people.add(new Person(24, "Оля"));
+        people.add(new Person(55, "Вася"));
+        people.add(new Person(63, "Маша"));
+
+        for (Person person : people) {
+            System.out.print(person.getAge() + " ");
+        }
+        System.out.println();
+
+        for (Person person : people) {
+            System.out.print(person.getName() + " ");
+        }
+
+        for (int i = 0; i < people.size(); i++) {
+            System.out.println(people.get(i).toString());
+        }
+    }
+}
