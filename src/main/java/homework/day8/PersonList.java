3622b1be36083aad1402ce799d040f362d59f4c3
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-25 16:46:30 +0300

    added homework day 8 Person

---
+++ src/main/java/homework/day8/PersonList.java
@@ -0,0 +1,20 @@
+package homework.day8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PersonList {
+    public static void main(String[] args) {
+       List<Person> people = Arrays.asList(new Person("Вася", 13, Person.Sex.MAN), new Person("Катя", 28, Person.Sex.WOMEN), new Person("Вова", 24, Person.Sex.MAN), new Person("Маша", 38, Person.Sex.WOMEN), new Person("Роман Петрович", 72, Person.Sex.MAN));
+
+        System.out.println(people.stream().filter(person -> person.age > 18 && person.age < 55 && person.sex.equals(Person.Sex.WOMEN) || person.age > 18 && person.age < 60 && person.sex.equals(Person.Sex.MAN)).count());
+
+        int count = 0;
+        for (Person person : people) {
+            if (person.age > 18 && person.age < 55 && person.sex.equals(Person.Sex.WOMEN) || person.age > 18 && person.age < 60 && person.sex.equals(Person.Sex.MAN)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
