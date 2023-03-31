02b1abbef88b204c84bfa7f41df7c0aad138e59a
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-31 21:48:49 +0300

    finished task 13 (water)

---
+++ src/main/java/homework/day9/WaterRunner.java
@@ -0,0 +1,28 @@
+package homework.day9;
+
+import homework.day7.objects.Water;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WaterRunner {
+    public static void main(String[] args) {
+        Stream<Water> water = Stream.of(
+                new Water("Прозрачная", "Нет"),
+                new Water("Прозрачная", "Нет"),
+                new Water("Мутная", "Аммиачный"),
+                new Water("Синяя", "Мятный"));
+
+        System.out.println(water.filter(s -> !s.getColor().equals("Прозрачная"))
+                .sorted((o1, o2) -> -o1.getSmell().compareTo(o2.getSmell()))
+                .flatMap(s -> Stream.generate(() -> {
+                    if (s.getSmell().contains("ы")) {
+                        return new Water(s.getColor(), s.getSmell().replace("ы", "ыы"));
+                    } else {
+                        return new Water(s.getColor(), s.getSmell());
+                    }
+                }).limit(1))
+                .map(Water::getSmell)
+                .collect(Collectors.joining()).length());
+    }
+}
