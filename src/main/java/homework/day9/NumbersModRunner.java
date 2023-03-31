469f16d9eebc977c36ad0577306dbe57359519b8
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-03-31 20:47:07 +0300

    finished task 9 (numbersMod)

---
+++ src/main/java/homework/day9/NumbersModRunner.java
@@ -0,0 +1,26 @@
+package homework.day9;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class NumbersModRunner {
+    public static void main(String[] args) {
+        Stream<Integer> numbersMod = Stream.of(626, 435, 9, 1463268, 24, 2237, 33, 9090);
+        final String[] numbers = {"нуль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+
+        numbersMod.map(String::valueOf)
+                .filter(s -> s.contains("3"))
+                .flatMap(s -> Arrays.stream(s.split("")))
+                .map(s -> {
+                            for (char c = '0'; c <= '9'; ++c)
+                                s = s.replaceAll(String.valueOf(c), numbers[c - '0'] + ' ');
+                            return s;
+                        }
+                )
+                .distinct()
+                .sorted(Collections.reverseOrder())
+                .forEach(System.out::println);
+    }
+}
