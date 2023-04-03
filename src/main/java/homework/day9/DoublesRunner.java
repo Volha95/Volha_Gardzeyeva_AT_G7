7fd351d0afa1373f05d4761fda44b02c8ba7066d
Volha_Gardzeyeva <volha.gardzeyeva@gmail.com>
2023-04-03 13:33:07 +0300

    corrected random, map

--- src/main/java/homework/day9/DoublesRunner.java
+++ src/main/java/homework/day9/DoublesRunner.java
@@ -1,17 +1,28 @@
 package homework.day9;
 
 import homework.day7.objects.Bubble;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class DoublesRunner {
     public static void main(String[] args) {
         Stream<Double> doubles = Stream.of(33.42, 34.3, 0.79, 2.3426, 6.8, 13.24, 5.5, 769.9);
+        Random random = new Random();
 
-        System.out.println(doubles.map(Math::round)
-                .map(s -> (int) (Math.random() * s.intValue()))
+        System.out.println(doubles.map(s -> (int) Math.round(s))
+                .map(random::nextInt)
                 .distinct()
-                .map(s -> new Bubble(s, "Bubble vol-" + s))
-                .peek(System.out::println)
-                .mapToInt(Bubble::getVolume).sum());
+                .map(s -> {
+                    List<Bubble> bubbles = new ArrayList<>();
+                    for (int i = 0; i < s; i++) {
+                        bubbles.add(new Bubble(s, "Bubble vol-" + s));
+                    }
+                    return bubbles.stream();
+                })
+                .mapToInt(s -> s.peek(System.out::println)
+                        .mapToInt(Bubble::getVolume).sum()).sum());
     }
 }
